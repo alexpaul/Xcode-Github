bd39b5f057e9f7ad459444cdbca87a1c4b8b362a
alexpaul <alexpaulnyc@gmail.com>
2018-10-05 14:54:26 -0400

    initial commit of my xcode project

---
+++ Xcode-Github/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Xcode-Github
+//
+//  Created by Alex Paul on 10/5/18.
+//  Copyright © 2018 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
